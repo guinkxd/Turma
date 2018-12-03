0a5cecfc663bf1d02c504d95081a5e800dc7a8e4
Aluno <Aluno@Aluno-THINK>
2018-12-03 17:39:31 -0200

    .

---
+++ src/Pacote/Classe.java
@@ -0,0 +1,11 @@
+package Pacote;
+
+public class Classe {
+	public static void main(String[] args) {
+		int x=2;
+		while(x==2) {
+			System.out.println("...........................................................");
+		}
+	}
+
+}
