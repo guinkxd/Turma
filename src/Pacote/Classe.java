231f872b596b89eb73ebc838c9e3fe5ad5422d93
Aluno <Aluno@Aluno-THINK>
2018-12-03 17:56:33 -0200

    fi nada

--- src/Pacote/Classe.java
+++ src/Pacote/Classe.java
@@ -2,10 +2,8 @@ package Pacote;
 
 public class Classe {
 	public static void main(String[] args) {
-		int x=2;
-		while(x==2) {
-			System.out.println("...........................................................");
-		}
+	
+	olha o geito q eu tol
 	}
 
 }
